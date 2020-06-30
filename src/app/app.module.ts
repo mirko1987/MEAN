5b113280f56740c9e0882f3842f58b05750f14cb
mirko uriel <mirkouriel@MBP-di-mirko.homenet.telecomitalia.it>
2020-06-30 18:28:48 +0200

    add  the first component and text area for the post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+  enteredValue='';
+  newPost='NO CONTENT';
+
+
+  onAddPost(){
+    console.log(this.enteredValue);
+    this.newPost=this.enteredValue;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
