1df871c7a80f273d2a2204d600b69ec14ec4ea9c
mirko uriel <mirkouriel@MBP-di-mirko.homenet.telecomitalia.it>
2020-07-06 22:58:26 +0200

    add server side get and post method

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,31 +1,33 @@
-import { PostsService } from './post.service';
-import { Component, OnInit, OnDestroy} from '@angular/core';
-import {Subscription} from 'rxjs';
-import { Post } from './post.model';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from 'rxjs';
+import { Post } from "./post.model";
+import {PostsService} from './post.service';
 
 @Component({
-  selector: 'app-post-list',
-  templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.css']
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
 })
-export class PostListComponent implements OnInit,OnDestroy {
+export class PostListComponent implements OnInit, OnDestroy {
+  // posts = [
+  //   { title: "First Post", content: "This is the first post's content" },
+  //   { title: "Second Post", content: "This is the second post's content" },
+  //   { title: "Third Post", content: "This is the third post's content" }
+  // ];
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
- posts:Post[]=[];
- private  postSub:Subscription;
-
-
-  constructor(public postService:PostsService) { }
+  constructor(public postsService: PostsService) {}
 
   ngOnInit() {
-    this.posts=this.postService.getPosts();
-    this.postService.getPostsUpdate()
-    .subscribe((posts:Post[])=>{
-      this.posts=posts;
-    });
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
   }
 
-  ngOnDestroy(){
-    this.postSub.unsubscribe();
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
   }
-
 }
