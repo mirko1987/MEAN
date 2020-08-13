a14c4c4ecf42cccef5b746f4680de499f1c9f9fb
mirko uriel <mirkouriel@MBP-di-mirko.home>
2020-08-13 20:01:28 +0200

    update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-
+import {Subject} from "rxjs";
 import { AuthData } from "./auth-data.model";
+
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  private isAuthenticated = false ;
   private token: string;
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener(){
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -21,12 +28,31 @@ export class AuthService {
       });
   }
 
+  getIsAuth(){
+    return this.isAuthenticated;
+  }
+
   login(email: string, password: string) {
     const authData: AuthData = {email: email, password: password};
     this.http.post<{token: string}>("http://localhost:3000/api/user/login", authData)
       .subscribe(response => {
-        const token = response.token;
+       const token = response.token;
         this.token = token;
+        if(token){
+          this.isAuthenticated = true;
+          this.authStatusListener.next(true);
+          this.router.navigate(['/']);
+
+        }
+
+
       })
   }
+  logout(){
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
+    this.router.navigate(['/']);
+
+   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../auth/auth.service';
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { PageEvent } from "@angular/material";
 import { Subscription } from "rxjs";
@@ -22,9 +23,11 @@ export class PostListComponent implements OnInit, OnDestroy {
   postsPerPage = 2;
   currentPage = 1;
   pageSizeOptions = [1, 2, 5, 10];
+  userIsAuthenticated = false;
   private postsSub: Subscription;
+  private authStatusSub:Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService,private authService:AuthService) {}
 
   ngOnInit() {
     this.isLoading = true;
@@ -35,7 +38,14 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.totalPosts = postData.postCount;
         this.posts = postData.posts;
+        this.userIsAuthenticated = this.authService.getIsAuth();
+        this.authStatusSub = this.authService.getAuthStatusListener().subscribe(IsAuthenticated=>{
+          this.userIsAuthenticated  = IsAuthenticated;
+        });
+
       });
+
+
   }
 
   onChangedPage(pageData: PageEvent) {
@@ -54,5 +64,6 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
   }
 }
